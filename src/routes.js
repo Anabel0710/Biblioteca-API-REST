1ff86d33b1bec38cf40ac39ac929fa6ae38873a5
Rocío Anabel <rocioanabelgonzalez07@gmail.com>
2023-10-26 00:41:11 -0300

    cambios en routes

--- src/routes.js
+++ src/routes.js
@@ -6,4 +6,6 @@ export const router = Router()
 router.get('/libros', libro.getAll);
 router.get('/libro', libro.getOne);
 router.post('/libros', libro.create);
-//router.post('/libro', libro.add);
+router.post('/libro', libro.add);
+router.delete('/libroid', libro.deleteid);
+router.put('/libro', libro.update);
