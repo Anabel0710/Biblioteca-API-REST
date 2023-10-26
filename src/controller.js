2fbeb0ca3255f50c49fc575e1e7fcf26286fe4f9
Rocío Anabel <rocioanabelgonzalez07@gmail.com>
2023-10-26 01:13:31 -0300

    correccion de controller

--- src/controller.js
+++ src/controller.js
@@ -1,41 +1,38 @@
 import { pool } from './database.js';
 class LibroController {
-    // obtenemos todo lo que tengamos en la BD con el metodo "getAll"
     async getAll(req, res) {
-        const [result] = await pool.query('SELECT * FROM libros');
-        res.json(result);
-    }
-
-    // obtenemos los datos de un registro a partir del id
-    async getOne(req, res) {
-        const libro = req.body;
-        const id_libro = parseInt(libro.id);
-        const [result] = await pool.query(`SELECT * FROM libros WHERE id=(?)`, [id_libro]);
-
-        // si el id coincide con la bd, muestra los datos, caso contrario, muestra un error
-        if (result[0] != undefined) {
-            res.json(result);
-        } else {
-            res.json({ "Error": "Ups, no se ha encontrado el ID del libro" });
-        }
-    }
-
-    async create(req, res) {
         try {
-            const { nombre, autor, categoria, isbn } = req.body;
-            const añoPublicacion = req.body['año-publicacion']; // Accedemos a la propiedad con corchetes
-
-            const sql = 'INSERT INTO Libros (nombre, autor, categoria, año_publicacion, isbn) VALUES (?, ?, ?, ?, ?)';
-            const values = [nombre, autor, categoria, añoPublicacion, isbn]; // Usamos la variable creada
-
-            await pool.query(sql, values);
-
-            res.status(201).json({ message: 'Libro agregado con éxito' });
+            const [result] = await pool.query('SELECT * FROM libros');
+            res.json(result);
         } catch (error) {
-            console.error("Error al agregar el libro:", error);
-            res.status(500).json({ error: "Error al agregar el libro" });
+            res.status(500).json({"Error": "Ocurrio un error al obtener los libros"});
         }
     }
+
+    async getOne(req, res) {
+        try {
+            const libro = req.body;
+            const [result] = await pool.query(`SELECT * FROM libros WHERE isbn=?`, [libro.isbn]);
+            if (result.length > 0) {
+                res.json(result[0]);
+            } else {
+                res.status(404).json({ "Error": "Ups, no se ha encontrado el ISBN ${libro.isbn}" });
+            }
+        } catch(error){
+            res.status(500).json({"Error":"Ocurrio un error al obtener los libros"});
+        }
+    }
+    
+    async add(req, res) {
+        try {
+            const libro = req.body;
+            const [result] = await pool.query('INSERT INTO Libros (nombre, autor, categoria, año_publicacion, isbn) VALUES (?, ?, ?, ?, ?)', [libro.nombre, libro.autor, libro.categoria, libro.añoPublicacion, libro.isbn]);
+            res.json({"Id insertado":result.insertid, "message": "Libro agregado con éxito"});        
+        } catch(error){
+            res.status(500).json("Ocurrio un error al agregar el libro")
+        }
+    }
+
 }
 
 export const libro = new LibroController();
